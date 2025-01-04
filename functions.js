f4dd8337e5606cda1eb7b365234f932748ee4f7c
dominam036 <dominik.namenyi@gmail.com>
2025-01-04 15:42:40 +0100

    tomb bevezetese

--- functions.js
+++ functions.js
@@ -1,49 +1,43 @@
-// HeaderObj objektum létrehozása
-const headerObj = {
-    thHm: "Harc megnevezése", // HeaderObj thHm tulajdonságának értékadása
-    thSzf: "Szembenálló felek", // HeaderObj thSzf tulajdonságának értékadása
-    thHr: "Haderő" // HeaderObj thHr tulajdonságának értékadása
-};
 
-// Adatsorok objektumainak létrehozása
-const Row1Element = {
-    tdHm: "Rákóczi szabadságharc", // Row1Element tdHm tulajdonságának értékadása
-    tdSzf: "Kuruc", // Row1Element tdSzf tulajdonságának értékadása
-    tdHr: "70.000", // Row1Element tdHr tulajdonságának értékadása
-};
-
-const Row2Element = {
-    tdSzf: "Labanc", // Row2Element tdSzf tulajdonságának értékadása
-    tdHr: "60.000" // Row2Element tdHr tulajdonságának értékadása
-};
-
-const Row3Element = {
-    tdHm: "48-as szabadságharc", // Row3Element tdHm tulajdonságának értékadása
-    tdSzf: "Osztrák császárság (+ Orosz birodalom)", // Row3Element tdSzf tulajdonságának értékadása
-    tdHr: "170.000 (+ 200.000)", // Row3Element tdHr tulajdonságának értékadása
-};
-
-const Row4Element = {
-    tdSzf: "Magyar királyság", // Row4Element tdSzf tulajdonságának értékadása
-    tdHr: "170.000" // Row4Element tdHr tulajdonságának értékadása
-};
-
-const Row5Element = {
-    tdHm: "I. világháború", // Row5Element tdHm tulajdonságának értékadása
-    tdSzf: "Antant", // Row5Element tdSzf tulajdonságának értékadása
-    tdHr: "43 millió", // Row5Element tdHr tulajdonságának értékadása
-};
-
-const Row6Element = {
-    tdSzf: "Központi hatalmak", // Row6Element tdSzf tulajdonságának értékadása
-    tdHr: "25 millió" // Row6Element tdHr tulajdonságának értékadása
-};
-
-const Row7Element = {
-    tdHm: "Bosworthi csata", // Row7Element tdHm tulajdonságának értékadása
-    tdSzf: "Angolok (York + Lancester)", // Row7Element tdSzf tulajdonságának értékadása
-    tdHr: "15.000" // Row7Element tdHr tulajdonságának értékadása
-};
+const array = [
+    { // array tomb nulladik elemenek letrehozasa
+        thHm: 'Harc megnevezése', // array tomb nulladik elem thHm tulajdonsaganak ertekadas
+        thSzf: "Szembenálló felek", // // array tomb nulladik elem thSzf tulajdonsaganak ertekadas
+        thHr: "Haderő", // // array tomb nulladik elem thHr tulajdonsaganak ertekadas
+    },
+    { // array tomb elso elemenek letrehozasa
+        tdHm: 'Rákóczi szabadságharc', // array tomb elso elem tdHm tulajdonsaganak ertekadas
+        tdSzf: "Kuruc", // // array tomb elso elem tdSzf tulajdonsaganak ertekadas
+        tdHr: "70.000", // // array tomb elso elem tdHr tulajdonsaganak ertekadas
+    },
+    { // array tomb masodik elemenek letrehozasa
+        tdSzf : 'Labanc', // array tomb masodik elem tdSzf tulajdonsaganak ertekadas
+        tdHr: '60.000', // array tomb masodik elem tdHr tulajdonsaganak ertekadas
+    },
+    { // array tomb harmadik elemenek letrehozasa
+        tdHm: '48-as szabadságharc', // array tomb harmadik elem tdHm tulajdonsaganak ertekadas
+        tdSzf : 'Osztrák császárság (+ Orosz birodalom)', // array tomb harmadik elem tdSzf tulajdonsaganak ertekadas
+        tdHr: '170.000 (+ 200.000)', // array tomb harmadik elem tdHr tulajdonsaganak ertekadas
+    },
+    { // array tomb negyedik elemenek letrehozasa
+        tdSzf : 'Magyar királyság', // array tomb negyedik elem tdSzf tulajdonsaganak ertekadas
+        tdHr: '170.000', // array tomb negyedik elem tdHr tulajdonsaganak ertekadas
+    },
+    { // array tomb otodik elemenek letrehozasa
+        tdHm: 'I. világháború', // array tomb otodik elem tdHm tulajdonsaganak ertekadas
+        tdSzf : 'Antant', // array tomb otodik elem tdSzf tulajdonsaganak ertekadas
+        tdHr: '43 millió', // array tomb otodik elem tdHr tulajdonsaganak ertekadas
+    },
+    { // array tomb hatodik elemenek letrehozasa
+        tdSzf : 'Központi hatalmak', // array tomb hatodik elem tdSzf tulajdonsaganak ertekadas
+        tdHr: '25 millió', // array tomb hatodik elem tdHr tulajdonsaganak ertekadas
+    },
+    { // array tomb hetedik elemenek letrehozasa
+        tdHm: 'Bosworthi csata', // array tomb hetedik elem tdHm tulajdonsaganak ertekadas
+        tdSzf : 'Angolok (York + Lancester)', // array tomb hetedik elem tdSzf tulajdonsaganak ertekadas
+        tdHr: '15.000', // array tomb hetedik elem tdHr tulajdonsaganak ertekadas
+    }
+]
 
 const table = document.createElement("table");//table elem létrehozása
 document.body.appendChild(table);//table elem hozzáadása a document.body-hoz
@@ -55,15 +49,15 @@ const h_row = document.createElement("tr");//h_row elem létrehozása
 thead.appendChild(h_row);//h_row elem hozzáadása a thead-hoz
 
 const th_hm = document.createElement("th");//th_hm elem létrehozása
-th_hm.innerHTML = headerObj.thHm;//th_hm szövegének megadása a rowobjectum tulajdonságával
+th_hm.innerHTML = array[0].thHm;//th_hm szövegének megadása a rowobjectum tulajdonságával
 h_row.appendChild(th_hm);//th_hm elem hozzáadása a h_row-hoz
 
 const th_szf = document.createElement("th");//th_szf elem létrehozása
-th_szf.innerHTML = headerObj.thSzf;//th_szf szövegének megadása a rowobjectum tulajdonságával
+th_szf.innerHTML = array[0].thSzf;//th_szf szövegének megadása a rowobjectum tulajdonságával
 h_row.appendChild(th_szf);//th_szf elem hozzáadása a h_row-hoz
 
 const th_hr = document.createElement("th");//th_hr elem létrehozása
-th_hr.innerHTML = headerObj.Hr;//th_hr szövegének megadása a rowobjectum tulajdonságával
+th_hr.innerHTML = array[0].thHr;//th_hr szövegének megadása a rowobjectum tulajdonságával
 h_row.appendChild(th_hr);//th_hr elem hozzáadása a h_row-hoz
 
 const tbody = document.createElement("tbody");//tbody elem létrehozása
@@ -92,75 +86,75 @@ tbody.appendChild(t_row7);//t_row7 elem hozzáadása a tbody-hoz
 
 const td_szf = document.createElement("td");//td_szf elem létrehozása
 td_szf.rowSpan = 2;//td_szf rowspanjának 2-re állítása
-td_szf.innerHTML =Row1Element.tdSzf;//td_szf szövegének megadása a rowobjectum tulajdonságával
+td_szf.innerHTML =array[1].tdSzf;//td_szf szövegének megadása a rowobjectum tulajdonságával
 t_row.appendChild(td_szf);//td_szf elem hozzáadása a t_row-hoz
 
 const td_hm = document.createElement("td");//td_hm elem létrehozása
-td_hm.innerHTML = Row1Element.tdHm;//td_hm szövegének megadása a rowobjectum tulajdonságával
+td_hm.innerHTML = array[1].tdHm;//td_hm szövegének megadása a rowobjectum tulajdonságával
 t_row.appendChild(td_hm);//td_hm elem hozzáadása a t_row-hoz
 
 const td_hr = document.createElement("td");//td_hr elem létrehozása
-td_hr.innerHTML = Row1Element.tdHr;//td_hr szövegének megadása a rowobjectum tulajdonságával
+td_hr.innerHTML = array[1].tdHr;//td_hr szövegének megadása a rowobjectum tulajdonságával
 t_row.appendChild(td_hr);//td_hr elem hozzáadása a t_row-hoz
 
 const td_szf2 = document.createElement("td");//td_szf2 elem létrehozása
-td_szf2.innerHTML = Row2Element.tdSzf;//td_szf2 szövegének megadása a rowobjectum tulajdonságával
+td_szf2.innerHTML = array[2].tdSzf;//td_szf2 szövegének megadása a rowobjectum tulajdonságával
 t_row2.appendChild(td_szf2);//td_szf2 elem hozzáadása a t_row2-hoz
 
 const td_hr2 = document.createElement("td");//td_hr2 elem létrehozása
-td_hr2.innerHTML = Row2Element.tdHr;//td_hr2 szövegének megadása a rowobjectum tulajdonságával
+td_hr2.innerHTML = array[2].tdHr;//td_hr2 szövegének megadása a rowobjectum tulajdonságával
 t_row2.appendChild(td_hr2);//td_hr2 elem hozzáadása a t_row2-hoz
 
 const td_szf3 = document.createElement("td");//td_szf3 elem létrehozása
 td_szf3.rowSpan = 2;//td_szf3 rowspanjának 2-re állítása
-td_szf3.innerHTML = Row3Element.tdSzf;//td_szf3 szövegének megadása a rowobjectum tulajdonságával
+td_szf3.innerHTML = array[3].tdSzf;//td_szf3 szövegének megadása a rowobjectum tulajdonságával
 t_row3.appendChild(td_szf3);//td_szf3 elem hozzáadása a t_row3-hoz
 
 const td_hm3 = document.createElement("td");//td_hm3 elem létrehozása
-td_hm3.innerHTML = Row3Element.tdHm;//td_hm3 szövegének megadása a rowobjectum tulajdonságával
+td_hm3.innerHTML = array[3].tdHm;//td_hm3 szövegének megadása a rowobjectum tulajdonságával
 t_row3.appendChild(td_hm3);//td_hm3 elem hozzáadása a t_row3-hoz
 
 const td_hr3 = document.createElement("td");//td_hr3 elem létrehozása
-td_hr3.innerHTML = Row3Element.tdHr;//td_hr3 szövegének megadása a rowobjectum tulajdonságával
+td_hr3.innerHTML = array[3].tdHr;//td_hr3 szövegének megadása a rowobjectum tulajdonságával
 t_row3.appendChild(td_hr3);//td_hr3 elem hozzáadása a t_row3-hoz
 
 const td_szf4 = document.createElement("td");//td_szf4 elem létrehozása
-td_szf4.innerHTML = Row4Element.tdSzf;//td_szf4 szövegének megadása a rowobjectum tulajdonságával
+td_szf4.innerHTML = array[4].tdSzf;//td_szf4 szövegének megadása a rowobjectum tulajdonságával
 t_row4.appendChild(td_szf4);//td_szf4 elem hozzáadása a t_row4-hoz
 
 const td_hr4 = document.createElement("td");//td_hr4 elem létrehozása
-td_hr4.innerHTML = Row4Element.tdHr;//td_hr4 szövegének megadása a rowobjectum tulajdonságával
+td_hr4.innerHTML = array[4].tdHr;//td_hr4 szövegének megadása a rowobjectum tulajdonságával
 t_row4.appendChild(td_hr4);//td_hr4 elem hozzáadása a t_row4-hoz
 
 const td_szf5 = document.createElement("td");//td_szf5 elem létrehozása
 td_szf5.rowSpan = 2;//td_szf5 rowspanjának 2-re állítása
-td_szf5.innerHTML = Row5Element.tdSzf;//td_szf5 szövegének megadása a rowobjectum tulajdonságával
+td_szf5.innerHTML = array[5].tdSzf;//td_szf5 szövegének megadása a rowobjectum tulajdonságával
 t_row5.appendChild(td_szf5);//td_szf5 elem hozzáadása a t_row5-hoz
 
 const td_hm5 = document.createElement("td");//td_hm5 elem létrehozása
-td_hm5.innerHTML = Row5Element.tdHm;//td_hm5 szövegének megadása a rowobjectum tulajdonságával
+td_hm5.innerHTML = array[5].tdHm;//td_hm5 szövegének megadása a rowobjectum tulajdonságával
 t_row5.appendChild(td_hm5);//td_hm5 elem hozzáadása a t_row5-hoz
 
 const td_hr5 = document.createElement("td");//td_hr5 elem létrehozása
-td_hr5.innerHTML = Row5Element.tdHr;//td_hr5 szövegének megadása a rowobjectum tulajdonságával
+td_hr5.innerHTML = array[5].tdHr;//td_hr5 szövegének megadása a rowobjectum tulajdonságával
 t_row5.appendChild(td_hr5);//td_hr5 elem hozzáadása a t_row5-hoz
 
 const td_szf6 = document.createElement("td");//td_szf6 elem létrehozása
-td_szf6.innerHTML = Row6Element.tdSzf;//td_szf6 szövegének megadása a rowobjectum tulajdonságával
+td_szf6.innerHTML = array[6].tdSzf;//td_szf6 szövegének megadása a rowobjectum tulajdonságával
 t_row6.appendChild(td_szf6);//td_szf6 elem hozzáadása a t_row6-hoz
 
 const td_hr6 = document.createElement("td");//td_hr6 elem létrehozása
-td_hr6.innerHTML = Row6Element.tdHr;//td_hr6 szövegének megadása a rowobjectum tulajdonságával
+td_hr6.innerHTML = array[6].tdHr;//td_hr6 szövegének megadása a rowobjectum tulajdonságával
 t_row6.appendChild(td_hr6);//td_hr6 elem hozzáadása a t_row6-hoz
 
 const td_szf7 = document.createElement("td");//td_szf7 elem létrehozása
-td_szf7.innerHTML = Row7Element.tdSzf;//td_szf7 szövegének megadása a rowobjectum tulajdonságával
+td_szf7.innerHTML = array[7].tdSzf;//td_szf7 szövegének megadása a rowobjectum tulajdonságával
 t_row7.appendChild(td_szf7);//td_szf7 elem hozzáadása a t_row7-hoz
 
 const td_hm7 = document.createElement("td");//td_hm7 elem létrehozása
-td_hm7.innerHTML = Row7Element.tdHm;//td_hm7 szövegének megadása a rowobjectum tulajdonságával
+td_hm7.innerHTML = array[7].tdHm;//td_hm7 szövegének megadása a rowobjectum tulajdonságával
 t_row7.appendChild(td_hm7);//td_hm7 elem hozzáadása a t_row7-hoz
 
 const td_hr7 = document.createElement("td");//td_hr7 elem létrehozása
-td_hr7.innerHTML = Row7Element.tdHr;//td_hr7 szövegének megadása a rowobjectum tulajdonságával
+td_hr7.innerHTML = array[7].tdHr;//td_hr7 szövegének megadása a rowobjectum tulajdonságával
 t_row7.appendChild(td_hr7);//td_hr7 elem hozzáadása a t_row7-hoz
